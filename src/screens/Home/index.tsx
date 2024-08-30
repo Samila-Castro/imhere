293d3fc20df58ed6a13192946640192d3254c48b
Samila-Castro <samilav1997@gmail.com>
2024-08-30 15:28:49 -0300

    Fundamentos - Lista de participants

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -34,6 +34,23 @@ export function Home() {
           </Text>
         </TouchableOpacity>
       </View>
+      <FlatList
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({item}) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipentRemove}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
     </View>
   );
 }
